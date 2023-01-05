f6cc5b1dc2d2505fb05e7652fba0625d037903dc
Vlad <druzhe.valdemar@gmail.com>
2023-01-05 17:24:36 +0200

    Sky

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ I make this game to learn more about React and Three js.
 
 ## todo list
 
-checked 
+- [x] Sky
+- [] Ground
+- [] Board
+    - [] Tiles
+    - [] Cards
+    - [] 3D buildings
+- [] Player
+- [] Dice
+- [] Player moves
--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,15 @@
-import { useState, useEffect } from 'react'
 import * as THREE from 'three';
-import { SpotLight } from 'three';
-import './App.css'
+import {Canvas} from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import styles from './App.module.css';
 
-function App() {
-  useEffect(() => {
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    
-    camera.position.z = 96;
-
-    const canvas = document.getElementById('ThreeJsCanvas');
-    const renderer = new THREE.WebGL1Renderer({
-      canvas,
-      antialias: true,
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
-
-    const animate = () => {
-      boxMesh.rotation.x += 0.01;
-      boxMesh.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
-    };
-
-    animate();
-  }, []);
+function App() {    
 
   return (
-    <div className="App">
-     <canvas id="ThreeJsCanvas" />
+    <div className={styles.App}>
+     <Canvas className={styles.canvas}> 
+      <Sky sunPosition={[100, 100, 20]} ></Sky>
+     </Canvas>
     </div>
   )
 }
